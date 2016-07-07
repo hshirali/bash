ae52b37e76d8e3e64989da3ba719b9a65d1e119a
hshirali <hshirali@gmail.com>
2016-07-07 21:02:25 +0430

    update from OC1 at Thu Jul  7 21:02:25 IRDT 2016

--- onos.bash
+++ onos.bash
@@ -22,10 +22,8 @@ onos-install -f $OC4
 
 s-byon-cell()
 {
-cd ~/onos-byon/
+cd ~/mybyon/onos-byon/
 onos-app 10.10.20.59 reinstall target/byon-1.0-SNAPSHOT.oar
-onos-app 10.10.20.60 reinstall target/byon-1.0-SNAPSHOT.oar
-onos-app 10.10.20.61 reinstall target/byon-1.0-SNAPSHOT.oar
 }
 
 s-restart-onos() {
