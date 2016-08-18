f7bb9b159c43b362a3281b6f0db2bf2f1cf92241
hshirali <hshirali@gmail.com>
2016-08-18 20:15:27 +0430

    update from OC1 at Thu Aug 18 20:15:27 IRDT 2016

--- onos.bash
+++ onos.bash
@@ -57,3 +57,54 @@ done
 
 alias shir-massstop=mymassstop
 alias shir-maswipe=mymasswipe
+
+s-init()
+{
+	onos-push-keys $OC2	
+	onos-push-keys $OC3
+	onos-push-keys $OC4
+	onos-patch-vm $OC2 OC2
+	onos-patch-vm $OC3 OC3
+	onos-patch-vm $OC4 OC4
+#	op
+#	onos-install $OC2
+#	onos-install $OC3
+#	onos-install $OC4
+}
+s-print-date()
+{
+	echo OC2
+	ssh sdn@$OC2 date
+	echo OC3
+        ssh sdn@$OC3 date
+	echo OC4
+        ssh sdn@$OC4 date
+}
+
+s-installfromzip()
+{
+for var in "$@"
+do
+	echo $var pre
+	onos-push-keys $var
+        scp /root/Downloads/onos-1.6.0.tar.gz  sdn@$var:/tmp/
+	echo $var ssh b
+	ssh sdn@$var "
+	sudo mkdir /opt/onos
+	sudo tar zxf /tmp/onos-1.6.0*.tar.gz -C /opt/onos --strip-components=1
+	sudo sed -i \"\\\$i /opt/onos/bin/onos-service server& \" /etc/rc.local
+	sudo apt-get install htop -y
+	"
+	echo $var ssh end
+done
+}
+
+s-test()
+{
+	ssh sdn@$@ "
+	echo \'test\'
+        sudo sed -i \"\\\$i /opt/onos/bin/onos-service server& \" /etc/rc.local
+	"
+}
+
+
